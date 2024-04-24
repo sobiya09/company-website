325fbd5331d23c886ab99343668d607b826525a0
Sobiya Puvanendrarajah <168014020+sobiya09@users.noreply.github.com>
2024-04-24 22:36:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,64 @@
+//Set current year//
+
+const yearEl = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+
+yearEl.textContent = currentYear;
+
+
+// Make mobile navigation work//
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+// Sticky header//
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 720) {
+    this.document.body.classList.add("sticky");
+  } else {
+    this.document.body.classList.remove("sticky");
+  }
+});
+
+
+
+const container = document.querySelector(".container"),
+      pwShowHide = document.querySelectorAll(".showHidePw"),
+      pwFields = document.querySelectorAll(".password"),
+      signUp = document.querySelector(".signup-link"),
+      login = document.querySelector(".login-link");
+
+    //   js code to show/hide password and change icon
+    pwShowHide.forEach(eyeIcon =>{
+        eyeIcon.addEventListener("click", ()=>{
+            pwFields.forEach(pwField =>{
+                if(pwField.type ==="password"){
+                    pwField.type = "text";
+
+                    pwShowHide.forEach(icon =>{
+                        icon.classList.replace("uil-eye-slash", "uil-eye");
+                    })
+                }else{
+                    pwField.type = "password";
+
+                    pwShowHide.forEach(icon =>{
+                        icon.classList.replace("uil-eye", "uil-eye-slash");
+                    })
+                }
+            }) 
+        })
+    })
+
+    // js code to appear signup and login form//
+    signUp.addEventListener("click", ( )=>{
+        container.classList.add("active");
+    });
+    login.addEventListener("click", ( )=>{
+        container.classList.remove("active");
+    });
+
